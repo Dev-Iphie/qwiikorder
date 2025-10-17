9a33f69296045d64d1e73a15b80ad9e4cb147e54
Dev-Iphie <ifeanyiagu.c@gmail.com>
2025-10-17 13:07:13 +0100

    created neworder page

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Footer } from "@/Components/Footer";
+import { Navbar } from "@/Components/Navbar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +23,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,42 +1,41 @@
 import Image from "next/image";
 
-
 export default function Home() {
   return (
     <main>
-      <p className="text-4xl text-orange-600 font-bold text-center mt-5">Welcome to QWIIKORDER</p>
-      <div className="flex justify-between px-40 py-15">
-        <div className="w-[400px] h-[300px]">
+      <div className="text-4xl text-orange-600 font-bold text-center mt-5 py-5">Welcome to QWIIKORDER</div>
+      <div>
           <p className="text-2xl font-bold text-center py-8">The Tailoring Struggle Ends Here</p>
           <p className="px-3 text-center">No more late deliveries, wrong sizes, or excuses. QwiikOrder connects you with professional tailors who deliver beautiful outfits, on time, every time.</p>
-        <div className="flex justify-center py-10" >
+          <div className="flex justify-center py-10">
           <p className="w-[200px] h-[40px] bg-orange-600 rounded-md text-white font-bold flex justify-center items-center">Place Your Order</p>
-        </div>
-        </div>
+      </div>
+      </div>
+      <div className="grid grid-cols-1 px-10 py-10 md:grid-cols-2 md:px-15 py-5 lg:grid-cols-2 lg:px-30">
         <div>
           <Image
       src="/sad.jpg"
       alt="sad"
       width={300}
-      height={250}/>
+      height={350} className="w-[300px] h-[300px] rounded-full my-3"/>
         </div>
         <div>
            <Image
       src="/happy0.jpg"
       alt="happy"
       width={300}
-      height={250}/>
+      height={300} className="w-[300px] h-[300px] rounded-full my-3"/>
         </div>
-      </div>
-      <p className="text-2xl font-bold flex justify-center">Your Perfect Fit In 4 Simple Steps</p>
-      <div className="flex justify-between py-7 px-25">
+      </div>  
+      <p className="text-2xl font-bold py-10 flex justify-center md:flex md:justify-center">Your Perfect Fit In 4 Simple Steps</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
       <div className="flex flex-col justify-center items-center mt-10">
         <Image
       src="/icons8-scissors-99.png"
       alt="custom"
       width={50}
       height={50}/>
-       <p class="pt-2">Custom Designs</p>
+       <p className="pt-2">Custom Designs</p>
       </div>
       <div className="flex flex-col justify-center items-center mt-10">
         <Image
@@ -44,7 +43,7 @@ export default function Home() {
       alt="happy"
       width={50}
       height={50}/>
-       <p class="pt-2">Perfect Fit Guarantee</p>
+       <p className="pt-2">Perfect Fit Guarantee</p>
       </div>
       <div className="flex flex-col justify-center items-center mt-10">
         <Image
@@ -52,7 +51,7 @@ export default function Home() {
       alt="happy"
       width={50}
       height={50}/>
-       <p class="pt-2">On Time Delivery</p>
+       <p className="pt-2">On Time Delivery</p>
       </div>
       <div className="flex flex-col justify-center items-center mt-10">
         <Image
@@ -60,33 +59,33 @@ export default function Home() {
       alt="happy"
       width={50}
       height={50}/>
-       <p class="pt-2">Quality Fabrics</p>
+       <p className="pt-2">Quality Fabrics</p>
       </div>
       </div>
-      <p className="text-2xl font-bold flex justify-center">See What We Have Created</p>
-      <div className="flex justify-between py-7 px-25">
-        <div>
+      <p className="text-2xl font-bold py-10 flex justify-center md:flex md:justify-center">See What We Have Created</p>
+      <div className="grid grid-cols-1 px-16 md:grid-cols-2 lg:grid-cols-4">
+        <div className="my-3">
           <Image
       src="/d1.jpg"
       alt="stressed"
       width={300}
       height={250}/>
         </div>
-         <div>
+         <div className="my-3">
           <Image
       src="/d2.jpg"
       alt="stressed"
       width={300}
       height={250}/>
         </div>
-         <div>
+         <div className="my-3">
           <Image
       src="/d3.jpeg"
       alt="stressed"
       width={300}
       height={250}/>
         </div>
-         <div>
+         <div className="my-3">
           <Image
       src="/d4jpg.jpg"
       alt="stressed"
